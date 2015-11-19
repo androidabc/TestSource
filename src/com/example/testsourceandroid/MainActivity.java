1f4c2be08a9cbe571a7993f04f818dadb84855a6
SourceTree <742062993@qq.com>
2015-11-19 19:35:27 +0800

    修改主界面

--- src/com/example/testsourceandroid/MainActivity.java
+++ src/com/example/testsourceandroid/MainActivity.java
@@ -12,16 +12,13 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-	//测试第一次提交的项目111
-	//测试第二次提交项目
-	//测试第三次提交项目
-	//测试第四次提交项目 sss
-	//测试第五次提交项目 sss
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+		
+		
 	}
 
 }
